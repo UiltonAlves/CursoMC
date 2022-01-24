33a91446256be7260aae875b357faaeb219c6bfc
Uilton Alves <uilnew18@gmail.com>
2022-01-24 14:12:58 -0300

    Confgurando tratamento de error com Handler

--- src/main/java/com/agenciakriativa/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/agenciakriativa/cursomc/resources/CategoriaResources.java
@@ -20,9 +20,10 @@ public class CategoriaResources {
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
-		
-		return ResponseEntity.ok().body(obj);	
+			Categoria obj = service.buscar(id);
+			
+			return ResponseEntity.ok().body(obj);	
+
 	}
 
 }
--- src/main/java/com/agenciakriativa/cursomc/services/CategoriaService.java
+++ src/main/java/com/agenciakriativa/cursomc/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.agenciakriativa.cursomc.domain.Categoria;
+import com.agenciakriativa.cursomc.exceptions.ObjectNotFoundException;
 import com.agenciakriativa.cursomc.repositories.CategoriaRepository;
 
 @Service
@@ -15,8 +16,9 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		Optional<Categoria> obj = repo.findById(id);	
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
