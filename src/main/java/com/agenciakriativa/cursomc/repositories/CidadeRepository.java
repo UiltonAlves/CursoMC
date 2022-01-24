f9828a9b1538c12d0cca37b6fe0f8a6b15a60740
Uilton Alves <uilnew18@gmail.com>
2022-01-24 14:32:39 -0300

    Criação class Cidade e Estado

---
+++ src/main/java/com/agenciakriativa/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.agenciakriativa.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.agenciakriativa.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+	
+}
