1b488bd38d325c4e5e75577654198f140acc96b7
Uilton Alves <uilnew18@gmail.com>
2022-01-26 17:09:55 -0300

    Outros ajustes nas classes resources e services

--- src/main/java/com/agenciakriativa/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/agenciakriativa/cursomc/resources/CategoriaResources.java
@@ -22,9 +22,9 @@ public class CategoriaResources {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		
-			Categoria obj = service.buscar(id);
+			Categoria obj = service.find(id);
 			
 			return ResponseEntity.ok().body(obj);	
 
@@ -32,6 +32,29 @@ public class CategoriaResources {
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@RequestBody Categoria obj){
 		
 		obj = service.insert(obj);
 		
