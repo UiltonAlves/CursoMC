5a12b374333d30877a504f663c88c605a48ab548
Uilton Alves <uilnew18@gmail.com>
2022-01-26 18:01:31 -0300

    Paginas com paramentros opcionais na requisições

--- src/main/java/com/agenciakriativa/cursomc/services/CategoriaService.java
+++ src/main/java/com/agenciakriativa/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.agenciakriativa.cursomc.domain.Categoria;
@@ -14,14 +17,15 @@ import com.agenciakriativa.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);	
-		
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		Optional<Categoria> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert(Categoria obj) {
@@ -30,27 +34,32 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		
+
 		find(obj.getId());
-		
+
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
-		
+
 		try {
 			repo.deleteById(id);
-			
-		}catch(DataIntegrityViolationException e) {
-			
+
+		} catch (DataIntegrityViolationException e) {
+
 			throw new DataIntegrityException("Não e possivel excluir uma categoria que possue produtos!");
 		}
-		
+
 	}
 
 	public List<Categoria> findAll() {
-		
+
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
