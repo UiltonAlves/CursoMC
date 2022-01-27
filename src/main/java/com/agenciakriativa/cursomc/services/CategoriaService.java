91c8fb14341d3ae7cac0e2d3870e5f79f4b2366e
Uilton Alves <uilnew18@gmail.com>
2022-01-27 10:02:23 -0300

    Criando validação da categoria com dto

--- src/main/java/com/agenciakriativa/cursomc/services/CategoriaService.java
+++ src/main/java/com/agenciakriativa/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.agenciakriativa.cursomc.domain.Categoria;
+import com.agenciakriativa.cursomc.dto.CategoriaDTO;
 import com.agenciakriativa.cursomc.repositories.CategoriaRepository;
 import com.agenciakriativa.cursomc.services.exceptions.DataIntegrityException;
 import com.agenciakriativa.cursomc.services.exceptions.ObjectNotFoundException;
@@ -60,6 +61,11 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDto(CategoriaDTO objDto) {
+		
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
