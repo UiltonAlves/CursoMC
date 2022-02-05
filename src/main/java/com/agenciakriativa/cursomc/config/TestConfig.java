cbfb9f3ec56345f51c27c4da211a824a3db6b50f
Uilton Araujo <uilnew18@gmail.com>
2022-02-04 21:56:18 -0300

    Criando profile de teste

---
+++ src/main/java/com/agenciakriativa/cursomc/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.agenciakriativa.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.agenciakriativa.cursomc.services.DbService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DbService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbService.instantianteTesteDatabase();
+		
+		return true;
+	} 
+	
+
+}
