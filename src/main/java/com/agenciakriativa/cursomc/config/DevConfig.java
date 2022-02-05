4df3d8ac40575c32cb9220eeda4ae0934d268ecb
Uilton Alves <uilnew18@gmail.com>
2022-02-04 23:01:50 -0300

    Criando profile de desenvolvimento DB MYSQL

--- src/main/java/com/agenciakriativa/cursomc/config/DevConfig.java
+++ src/main/java/com/agenciakriativa/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.agenciakriativa.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,18 +11,24 @@ import org.springframework.context.annotation.Profile;
 import com.agenciakriativa.cursomc.services.DbService;
 
 @Configuration
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 	
 	
 	@Autowired
 	private DbService dbService;
 	
+	@Value("${Spring.jpa.hibernate.dll.auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		
-		dbService.instantianteTesteDatabase();
+		if(!"create".equals(strategy)) {
+			return false;
+		}
 		
+		dbService.instantianteTesteDatabase();
 		return true;
 	} 
 	
