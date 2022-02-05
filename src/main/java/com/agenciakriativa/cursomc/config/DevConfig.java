b4683c80ecc8ffea2feda73847b03cf6c54cc631
Uilton Alves <uilnew18@gmail.com>
2022-02-05 02:31:33 -0300

    Finalizado a implementação do envio de email,

--- src/main/java/com/agenciakriativa/cursomc/config/DevConfig.java
+++ src/main/java/com/agenciakriativa/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.agenciakriativa.cursomc.services.DbService;
+import com.agenciakriativa.cursomc.services.EmailService;
+import com.agenciakriativa.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -31,6 +33,11 @@ public class DevConfig {
 		dbService.instantianteTesteDatabase();
 		return true;
 	} 
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
 	
 
 }
